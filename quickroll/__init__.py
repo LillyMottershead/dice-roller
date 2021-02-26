b4ec26264d43dc514424d3ebc1f50e777f653c8d
LillyMottershead <lilliana.mottershead@gmail.com>
2021-02-25 19:44:23 -0500

    arguments for dice rolls in the parser, issues with alias adv/dis/max arguments

--- quickroll/__init__.py
+++ quickroll/__init__.py
@@ -1,29 +1,40 @@
 import os
 
 from flask import (
-    Flask, render_template, request
+    Flask, render_template, render_template, request, session
 )
 from .parser.dice_parser import parse_command
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     @app.route('/', methods=('GET', 'POST'))
     def index():
-        response = 'test'
+        if 'log' not in session:
+            session['log'] = []
         if request.method == 'POST':
-            command = request.form['command']
-            response = parse_command(command)[0]
-        return render_template('index.html', response=response)
+            if command := request.form.get('command'):
+                roll = parse_command(command)[0]
+                roll_list = roll.split('\n')
+                session['log'] = session.get('log') + roll_list
+            if thing := request.form.get('clear'):
+                session['log'] = []
+        return render_template('index.html')
 
     return app
--- quickroll/parser/dice_parser.py
+++ quickroll/parser/dice_parser.py
@@ -1,49 +1,69 @@
-from .dice import Dice
+from dice import Dice
+from inspect import signature
 import re
 
 
 blank_space_pattern = re.compile(r'(\s*)')
 single_dice_roll_pattern = re.compile(r'(?P<times>\d+)?d(?P<sides>\d+)(?:k(?P<keep>[l-]?\d*))?')
-valid_tokens = {'dice_roll': r'(?:(?:\d+)?d\d+(?:kl?\d*)?)',
-                'math_operator': r'(?:[+\-*/()])',
-                'number': r'(?:\d+)',
-                'label': r'(?:\[[\w\s]*\])',
-                'adv': r'(?:adv)',
-                'dis': r'(?:dis)'}
-valid_compound_tokens_pattern = re.compile('(' + '|'.join(valid_tokens.values()) + ')')
-complete_compound_dice_roll_pattern = re.compile('(' + '|'.join(valid_tokens.values()) + r'|(?:\s))+')
 args_pattern = re.compile(r'(?:(\w+)(?:\((.*?)\))?)')
+valid_tokens = {
+    'dice_roll': r'(?:(?:\d+)?d\d+(?:kl?\d*)?)',
+    'math_operator': r'(?:[+\-*/()])',
+    'number': r'(?:\d+)',
+    'label': r'(?:\[[\w\s]*\])',
+    'arg':  args_pattern.pattern,
+}
+valid_compound_tokens_pattern = re.compile('(' + '|'.join(valid_tokens.values()) + ')')
+valid_tokens.pop('arg')
+valid_compound_tokens_no_args_pattern = re.compile('(' + '|'.join(valid_tokens.values()) + ')')
+complete_compound_dice_roll_pattern = re.compile('(' + '|'.join(valid_tokens.values()) + r'|(?:\s))+')
 
 aliases = set()
 alias_to_code = {}
 
-
 def parse_single_dice_roll(token):
-    match = re.match(single_dice_roll_pattern, token)
-    times = int(match.group('times')) if match.group('times') is not None else 1
-    keep = int(match.group('keep').replace('l', '-')) if match.group('keep') is not None else 0
+    match = single_dice_roll_pattern.match(token)
+    times = int(match.group('times') or 1)
+    keep = int((match.group('keep') or '0').replace('l', '-'))
     return Dice(times, int(match.group('sides')), keep)
 
 
 def parse_compound_dice_roll(compound_dice_roll):
-    tokens = re.findall(valid_compound_tokens_pattern, compound_dice_roll)
-    if 'dis' in tokens:
-        d20_index, _ = next(enumerate([token for token in tokens if 'd20' in token]))
-        tokens[d20_index] = '2d20kl1'
-        tokens.remove('dis')
-    if 'adv' in tokens:
-        d20_index, _ = next(enumerate([token for token in tokens if 'd20' in token]))
-        tokens[d20_index] = '2d20k1'
-        tokens.remove('adv')
-    try:
-        label = list(filter(lambda x: re.match(valid_tokens['label'], x) is not None, tokens))[0]
-        tokens = list(filter(lambda x: re.match(valid_tokens['label'], x) is None, tokens))
-    except IndexError:
-        label = ''
+    tokens = valid_compound_tokens_pattern.findall(compound_dice_roll)
+    args = [token[1:] for token in tokens if not valid_compound_tokens_no_args_pattern.match(token[0])]
+    args = [(x[0], blank_space_pattern.sub('', x[1]).split(',')) for x in args]
+    tokens = [token[0] for token in tokens if valid_compound_tokens_no_args_pattern.match(token[0])]
+    for arg_name, arg_params in args:
+        roll_args_to_func[arg_name](tokens, *arg_params)
+    label = next(iter([token for token in tokens if re.match(valid_tokens['label'], token)]), '')
+    tokens = [token for token in tokens if not re.match(valid_tokens['label'], token)]
     tokens_with_dice_objects = list(map(lambda x: parse_single_dice_roll(x) if re.fullmatch(single_dice_roll_pattern, x) else x, tokens))
     tokens_with_dice_result_strings = list(map(str, tokens_with_dice_objects))
     tokens_with_dice_result_ints = list(map(lambda x: str(x.result) if type(x) is Dice else x, tokens_with_dice_objects))
     return ' '.join(tokens_with_dice_result_strings) + ' = ' + str(eval(''.join(tokens_with_dice_result_ints))) + ' ' + label[1:-1]
+
+
+def add(roll_tokens, extra_code):
+    if extra_code[0] not in ['-+']:
+        roll_tokens.append('+')
+    added_tokens = valid_compound_tokens_pattern.findall(extra_code)
+    added_tokens = [token[0] for token in added_tokens]
+    roll_tokens.extend(added_tokens)
+
+
+def replace(roll, repl_token, target):
+    for index, token in enumerate(roll):
+        if re.fullmatch(f'(?:\d+)?{target}(?:kl?\d*)?', token):
+            roll[index] = repl_token
+
+
+def max_dice(*roll):
+    roll = roll[0]
+    for index, token in enumerate(roll):
+        if match := single_dice_roll_pattern.fullmatch(token):
+            times = int(match.group('times') or 1)
+            sides = int(match.group('sides'))
+            roll[index] = str(times * sides)
 
 
 def parse_command(command, inner=False):
@@ -59,55 +79,32 @@ def parse_command(command, inner=False):
         except IndexError:
             args = []
         for arg_name, arg_params in args:
-            alias_args_to_func = {'add': add,
-                                  'addroll': add_roll,
-                                  'replace': replace,
-                                  'adv': lambda x, y: replace(x, '2d20k1', 'd20', y),
-                                  'dis': lambda x, y: replace(x, '2d20kl1', 'd20', y),
-                                  'max': max_dice,
-                                  'crit': max_dice}
-            alias_args_to_func[arg_name](code, *arg_params)
+            if arg_name in roll_args_to_func:
+                n_other_params = len(signature(roll_args_to_func[arg_name]).parameters)-1
+                alias_arg_specific_roll_wrapper(code, arg_name, n_other_params, arg_params)
+            else:
+                alias_args_to_func = {
+                    'addroll': alias_add_roll,
+                }
+                alias_args_to_func[arg_name](code, *arg_params)
         return '\n'.join(list(map(lambda x: parse_command(x)[0], code))), True
-    elif match := re.match(complete_compound_dice_roll_pattern, command):
-        return parse_compound_dice_roll(match.group()), None
+    elif complete_compound_dice_roll_pattern.match(command):
+        return parse_compound_dice_roll(command), None
     else:
-        return 'Something went wrong'
+        return 'Something went wrong',
+
+def alias_arg_specific_roll_wrapper(code, arg_name, n_other_params, params):
+    if len(params) == n_other_params:
+        which_roll = 0
+    else:
+        which_roll = int(params[-1]) - 1
+        params = params[:-1]
+    arg = f'{arg_name}({",".join(params)})'
+    code[which_roll] = ''.join((code[which_roll], arg))
 
 
-def add(code, extra_code, roll=1):
-    roll = int(roll)-1
-    if extra_code[0] not in ['-+']:
-        extra_code = '+' + extra_code
-    code[roll] = ''.join((code[roll], extra_code))
-
-
-def add_roll(code, new_roll):
+def alias_add_roll(code, new_roll):
     code.append(new_roll)
-
-
-def replace(code, repl_code, target, roll=1):
-    if roll == '':
-        roll = 1
-    roll = int(roll)-1
-    code[roll] = re.sub(f'(?:\d+)?{target}(?:kl?\d*)?', repl_code, code[roll])
-
-
-def max_dice(code, roll=1):
-    if roll == '':
-        roll = 1
-    roll = int(roll)-1
-    tokens = re.findall(valid_compound_tokens_pattern, code[roll])
-    tokens = list(map(lambda x: str((int(times) if (times := match.group('times')) else 1)*int(match.group('sides'))) if (match := single_dice_roll_pattern.fullmatch(x)) else x, tokens))
-    joined_tokens = ''.join(tokens)
-    code[roll] = ''.join(tokens)
-    return joined_tokens
-
-
-def parse_command_tuple_wrapper(command):
-    result = parse_command(command)
-    if type(result) != tuple:
-        return result, None
-    return result
 
 
 # returns
@@ -135,17 +132,27 @@ def alias_command(command):
                 return f'Added {second_arg} to alias list', second_arg
             except IndexError:
                 if second_arg in aliases:
-                    return f'{second_arg}: {alias_to_code[second_arg]}'
-                return f'No alias {second_arg}'
+                    return f'{second_arg}: {alias_to_code[second_arg]}',
+                return f'No alias {second_arg}',
     except IndexError:
         if len(aliases) == 0:
-            return f'No aliases'
-        return f'Aliases: {", ".join(list(aliases))}'
+            return f'No aliases',
+        return f'Aliases: {", ".join(list(aliases))}',
+
+
+roll_args_to_func = {
+    'add': add,
+    'replace': replace,
+    'adv': lambda x, y: replace(x, '2d20k1', 'd20'),
+    'dis': lambda x, y: replace(x, '2d20kl1', 'd20'),
+    'max': max_dice,
+    'crit': max_dice,
+    }
 
 
 if __name__ == "__main__":
+    print('Ctrl+C to terminate')
     while True:
         repl_input = input("?: ")
-        if repl_input == 'q':
-            exit()
         print(parse_command(repl_input)[0])
+        print()
